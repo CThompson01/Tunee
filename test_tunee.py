57c92fdb87f4e12a2129558785929c26b985c749
Chase Thompson <cthompson10547@gmail.com>
2024-06-12 23:31:29 -0500

    Add Skeleton Code for Tests
    Added skeleton code for the Youtube-DL test functions.

--- test_tunee.py
+++ test_tunee.py
@@ -8,9 +8,15 @@ import tunee
 
 TEST_AUDIO_DIR = 'test_audio/'
 TEST_AUDIO_LENGTHS = {
-	"mbf.wav": 1,
-	"context.wav": 12
+	'mbf.wav': 1,
+	'context.wav': 12
 }
+
+TEST_URLS = [{'string': "https://www.youtube.com/watch?v=n5YHJMdN9FE", 'expected': True},
+	{'string': "MBF", 'expected': False},
+	{'string': "beans beans beans", 'expected': False},
+	{'string': "https://www.youtube.com/watch?v=XBtALgPwTfo", 'expected': True}
+]
 
 @pytest.mark.parametrize("audio_file", os.listdir(TEST_AUDIO_DIR))
 def test_get_length(audio_file):
@@ -22,3 +28,20 @@ def test_get_length(audio_file):
 	length = tunee.get_length(f'{TEST_AUDIO_DIR}/{audio_file}')
 	expected_length = TEST_AUDIO_LENGTHS[audio_file]
 	assert expected_length <= length <= expected_length+1
+
+@pytest.mark.parametrize('yt_video', TEST_URLS)
+def test_dl_video(yt_video):
+	"""
+	Test the dl_video function by running through a list of YouTube videos
+	and check that the function is properly returning the file name and that
+	the file exists.
+	"""
+	assert False
+
+@pytest.mark.parametrize('check_string', TEST_URLS)
+def test_is_url(check_string):
+	"""
+	Checks that the is_url function is properly returning whether the provided
+	string is a URL or a search text.
+	"""
+	assert tunee.is_url(check_string['string']) is check_string['expected']
